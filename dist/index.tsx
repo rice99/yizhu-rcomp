599fafd59bd0aee4e09e88c43c71bc303293d473
kevin <946237542@qq.com>
2020-08-20 16:07:38 +0800

    Button

---
+++ dist/index.tsx
@@ -0,0 +1,4 @@
+export {default as Button} from "./components/button";
+export * from "./components/shared/global";
+export * from "./components/shared/style";
+export * from "./components/shared/animation";
