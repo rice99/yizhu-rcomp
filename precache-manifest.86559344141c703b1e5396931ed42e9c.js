92524b53710cc95e765a50a0e2d85d430ea1e1a9
rice99 <946237542@qq.com>
2020-08-20 09:41:54 +0000

    Deploying to gh-pages from  @ 969c2d9bcdaf76ae116aea8974981168a3365a40 🚀

---
+++ precache-manifest.86559344141c703b1e5396931ed42e9c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df60e43a8eead4f861bb0569951eae36",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.51f45c9ac47a1fd443c5.bundle.js"
+  },
+  {
+    "url": "runtime~main.51f45c9ac47a1fd443c5.bundle.js"
+  },
+  {
+    "url": "vendors~main.51f45c9ac47a1fd443c5.bundle.js"
+  },
+  {
+    "url": "vendors~main.51f45c9ac47a1fd443c5.bundle.js.LICENSE.txt"
+  }
+]);
