5ee9337607f40adcada7ca34ba54202f0b5d40cd
kevin <946237542@qq.com>
2020-08-20 17:38:17 +0800

    删除没用的文件

--- dist/App.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-function App() {
-    return (React.createElement("div", { className: "App" }, "hello"));
-}
-export default App;
--- dist/index.tsx
+++
@@ -1,4 +0,0 @@
-export {default as Button} from "./components/button";
-export * from "./components/shared/global";
-export * from "./components/shared/style";
-export * from "./components/shared/animation";
